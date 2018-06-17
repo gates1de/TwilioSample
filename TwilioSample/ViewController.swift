c21ab5d447e4b477a78e0616fde7d7c11501a07c
gates1de <g031i043@gmail.com>
2018-06-17 17:40:37 +0900

    :sparkles: Implement get twilio access token

--- TwilioSample/ViewController.swift
+++ TwilioSample/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import TwilioVoice
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // NOTE: Token includes '"', so remove '"'
+        guard let fetchedToken = try? TokenUtils.fetchToken(), let token = fetchedToken?.replacingOccurrences(of: "\"", with: "") else {
+            return
+        }
+
+        print("token = \(token)")
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +30,21 @@ class ViewController: UIViewController {
 
 }
 
+
+struct TokenUtils {
+    static func fetchToken() throws -> String? {
+        guard let tokenUrlString = ProcessInfo.processInfo.environment["TwilioTokenUrl"] else {
+            print("Cannot get tokenUrl!")
+            return nil
+        }
+
+        let requestUrl: URL = URL(string: tokenUrlString)!
+        do {
+            let data = try Data(contentsOf: requestUrl)
+            return String.init(data: data, encoding: String.Encoding.utf8)
+        } catch let error as NSError {
+            print ("Invalid token url, error = \(error)")
+            throw error
+        }
+    }
+}
